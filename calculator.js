8ac8c8115184496bd09660410431781b50b6739b
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-29 21:12:17 +0100

    correct formula BMI

--- calculator.js
+++ calculator.js
@@ -5,29 +5,28 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.get("/", function (req, res) {
-//   res.sendFile(__dirname + "/index.html")
-// })
-
-// app.post("/", function (req, res) {
-//   let num1 = Number(req.body.num1)
-//   let num2 = Number(req.body.num2)
-
-//   let result = num1 + num2
-//   res.send("The result of the calculation is " + result)
-
-// })
-
-app.get("/bmicalculator", function (req, res) {
-  res.sendFile(__dirname + "/bmiCalculator.html")
-  let weight = Number(req.body.weight)
-  let height = Number(req.body.height)
-
-  let result = weight / height * 100
-  res.send("The result of the calculation is " + result)
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html")
 })
 
 app.post("/", function (req, res) {
+  let num1 = Number(req.body.num1)
+  let num2 = Number(req.body.num2)
+
+  let result = num1 + num2
+  res.send("The result of the calculation is " + result)
+
+})
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", function (req, res) {
+  let weight = parseFloat(req.body.weight)
+  let height = parseFloat(req.body.height)
+  let result = weight / (weight * height)
+  res.send("Your BMI is " + result)
 
 })
 
