ad99b7a9f740d87ed85a7e9839cdfdcc7a8ffe9c
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-29 20:21:48 +0100

    creating structure of bMI calculator

--- calculator.js
+++ calculator.js
@@ -5,16 +5,29 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html")
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/index.html")
+// })
+
+// app.post("/", function (req, res) {
+//   let num1 = Number(req.body.num1)
+//   let num2 = Number(req.body.num2)
+
+//   let result = num1 + num2
+//   res.send("The result of the calculation is " + result)
+
+// })
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+  let weight = Number(req.body.weight)
+  let height = Number(req.body.height)
+
+  let result = weight / height * 100
+  res.send("The result of the calculation is " + result)
 })
 
 app.post("/", function (req, res) {
-  let num1 = Number(req.body.num1)
-  let num2 = Number(req.body.num2)
-
-  let result = num1 + num2
-  res.send("The result of the calculation is " + result)
 
 })
 
